65c440faed4d007aa99679b915b71c945cdb00ad
Alex H <08ahayden@gmail.com>
2018-02-13 18:04:53 -0500

    letter function done, word function returns dashes

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var Word = require('./word.js');
 var inquirer = require('inquirer');
+var possiblewords = ["Gandalf", "Valinor", "Sauron", "Galadriel", "Balrog", "Gondor", "Rohan", "Moria", "Shire", "Baggins", "Gollum", "Frodo", "Orc", "Troll", "Dwarf", "Fellowship", "Aragorn", "Boromir", "Legolas",
+"Merry", "Pippin", "Sam"];
+var chosenword;
+
+console.log(possiblewords);
+function chooseword () {
+  chosenword = secretwords[Math.floor(Math.random() * possiblewords.length)].toLowerCase();
+}
+
+chooseword();
+console.log(chosenword);
